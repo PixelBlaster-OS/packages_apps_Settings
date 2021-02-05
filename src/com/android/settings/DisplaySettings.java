44507a39c654a9bf8bdc208251d23bb2f0f264e5
madmax7896 <thegreatsagar123@gmail.com>
2021-02-05 11:38:53 +0000

    Revert "Settings: Add Icon pack customisation settings"
    This reverts commit 1fa70b2e2b7905860a8b0b7088db4e24561f93a5.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -104,8 +104,6 @@ public class DisplaySettings extends DashboardFragment {
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.icon_pack.android"));
         return controllers;
     }
 
